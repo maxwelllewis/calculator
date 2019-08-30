ca99772a88f740627f15742a3e8539612bd95e69
Josh Raglione <raglionejosh@gmail.com>
2019-08-29 17:05:08 -0700

    this will add subtraction. division, and multiplication to calculator.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,34 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var number1 = parseInt(prompt("add:"));
+var number2 = parseInt(prompt("add:"));
 var result = add(number1, number2);
+alert(result);
+
+var sub = function(number1, number2) {
+  return number1 - number2;
+};
+
+var number1 = parseInt(prompt("subtract:"));
+var number2 = parseInt(prompt("subtract:"));
+var result = sub(number1, number2);
+alert(result);
+
+var div = function(number1, number2) {
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("divide:"));
+var number2 = parseInt(prompt("divide:"));
+var result = div(number1, number2);
+alert(result);
+
+var multi = function(number1, number2) {
+  return number1 * number2;
+};
+
+var number1 = parseInt(prompt("multiply:"));
+var number2 = parseInt(prompt("multiply:"));
+var result = multi(number1, number2);
 alert(result);
